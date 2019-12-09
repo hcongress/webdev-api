33d6996f12f3bc2a7044d3626b36e62619917ee6
hcongress <hunterjc28@gmail.com>
2019-12-09 14:58:16 -0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <v-row
+          v-for="(item, i) in info"
+          :key="i"
+          class = "ma-5"
+        >
+      <v-card
+    :loading="loading"
+    class="mx-auto ma-5"
+    max-width="1000"
+    min-width="250"
+  >
+    <v-img
+      aspect-ratio = "1.77"
+      :src="item.imgMedium"
+    ></v-img>
+
+    <v-card-title>{{item.name}}</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="item.stars"
+          color="blue"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">{{item.stars}} ({{item.starVotes}})</div>
+      </v-row>
+
+      <div class="my-4 subtitle-1 black--text">
+        {{item.location}} <br>
+        Length: {{item.length}} miles  • Ascent: {{item.ascent}} ft • Descent: {{item.descent}} ft
+      </div>
+
+      <div>{{item.summary}} While the idea of skiing may inspire images of powdery snow, gorgeous vistas and steamy hot chocolate, it is important to remember that skiing is no walk in the park. It is, however, a thrilling sport that can satiate anyone's need for an adrenaline rush. If you've always wanted to try out skiing but have not yet had the opportunity to do so, this guide can help you get started. Keep in mind that while this article covers the basics of alpine ("downhill") skiing, it is not a substitute for actual lessons — read on and then go enroll in a class to begin to have ridiculous amounts of snowy fun!</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>{{item.difficulty}}</v-card-title>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple accent-4"
+        text
+        @click="reserve"
+      >
+        Reserve
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+
+  data () {
+    return {
+      info: []
+    }
+  },
+  backgroundColor: function (item) {
+    let rating = item.difficulty
+    if (rating === 'dblack') {
+      return '#000000'
+    } else if (rating === 'black') {
+      return '#4a4a4a'
+    } else if (rating === 'blueBlack') {
+      return '#000d61'
+    } else if (rating === 'blue') {
+      return '#0022ff'
+    } else {
+      return '#00c91b'
+    }
+  },
+  mounted () {
+    axios.get('https://www.powderproject.com/data/get-trails?lat=39.8917&lon=-105.7631&maxDistance=200&key=200648940-e0f4d3e6dd5f91b90142df34f1899279')
+      .then(response => {
+        console.log(response.data)
+        this.info = response.data.trails
+      })
+  }
+}
+</script>
